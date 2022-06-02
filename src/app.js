1172b50cb254405c65622aff4af5666ea824a706
ronakmystery <50558098+ronakmystery@users.noreply.github.com>
2022-06-02 01:42:39 -0500

    routing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Login } from "./components/login";
+import { Home } from "./components/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
