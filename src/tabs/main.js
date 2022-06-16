00e8b0497fd7898217e53e3699459f577891805b
ronakmystery <50558098+ronakmystery@users.noreply.github.com>
2022-06-16 00:28:39 -0500

    dead package

--- src/tabs/main.js
+++ src/tabs/main.js
@@ -9,36 +9,20 @@ import { motion } from "framer-motion";
 
 import "./main.scss";
 
-import CanvasDraw from "react-canvas-draw";
-
 export const Main = () => {
   const state = useSelector((state) => state);
-
-  const canvas = useRef();
-
 
   return (
     <motion.div id="main" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
       {!state.user ? <Login /> : <Home />}
 
-      {window.innerWidth> 1000 && state.user && (
+      {window.innerWidth > 1000 && state.user && (
         <div id="ipad">
           <div id="draw">
-            <CanvasDraw
-              ref={canvas}
-              hideGrid={true}
-              canvasWidth={window.innerWidth - 350}
-              hideInterface={true}
-              canvasHeight={window.innerHeight}
-              brushRadius={1}
-              enablePanAndZoom={true}
-              lazyRadius={0}
-            />
             <button
               className="erase"
               onClick={() => {
                 if (window.confirm("Clear canvas?")) {
-                  canvas.current.eraseAll();
                 }
               }}
             >
