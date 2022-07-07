14578079b9bef53478c597d3533f7c49ba983017
ronakmystery <50558098+ronakmystery@users.noreply.github.com>
2022-07-07 00:15:21 -0500

    sort by time

--- src/functions/time.js
+++ src/functions/time.js
@@ -7,3 +7,8 @@ export function convert24to12(time) {
 export function convertToHour(time) {
   return moment(time, ["hh.mm"]).format("hA");
 }
+
+export function convertToHalfHour(time) {
+  console.log(moment(time, ["hh.mm"]).minute());
+  // return moment(time, ["hh.mm"]).format("h:mm A");
+}
